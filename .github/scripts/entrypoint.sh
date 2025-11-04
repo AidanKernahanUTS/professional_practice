9ca907b6394b5b267d90393b447da04596a2176b
AidanKernahanUTS <aidan.kernahan@student.uts.edu.au>
2025-11-04 20:35:12 +1100

    Update entrypoint.sh

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-
-echo "Starting Frequency Analyzer..."
-
-
-#Running the Python frequency analyzer
-FREQ_RESULT=$(python3 /app/.github/scripts/frequency.py /app/data.txt)
-
-#Running update_readme.sh
-bash /app/.github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
+echo "===================="
+echo "Executing todo.py"
+echo "===================="
+python3 /app/.github/scripts/todo.py | tee /app/todo_output.txt
 
 
-echo "Process Completed!"
+echo "===================="
+echo "Executing todo-test.py"
+echo "===================="
+python3 /app/.github/scripts/todo-test.py | tee /app/test_output.txt
+
+
+echo "===================="
+echo "Executing update_index.sh"
+echo "===================="
+python3 /app/.github/scripts/update_index.sh /app/todo_output.txt /app/test_output.txt
+
+
+
+echo "===================="
+echo "Done!"
+echo "===================="
+
+
+# echo "Starting Frequency Analyzer..."
+
+
+# #Running the Python frequency analyzer
+# FREQ_RESULT=$(python3 /app/.github/scripts/frequency.py /app/data.txt)
+
+# #Running update_readme.sh
+# bash /app/.github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
+
+
+# echo "Process Completed!"
