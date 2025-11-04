f037f817477484fb1ee9cbe33efaa1b6ddc7298d
AidanKernahanUTS <aidan.kernahan@student.uts.edu.au>
2025-11-04 18:37:36 +1100

    configure update_readme.sh

--- .github/scripts/update_readme.sh
+++ .github/scripts/update_readme.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+#Inputs
+FREQ_RESULT=$1
+GITHUB_USER=$2
+TIMESTAMP=$(date)
+
+#update README
+echo -e "\n[$GITHUB_USER - $FREQ_RESULT - $TIMESTAMP]" >> README.md
+
+#configure git to use github actions user and email
+git config --global user.name "github.actions"
+git config --global user.email "github-actions@users.noreply.github.com"
+
+
+#add README.mb to git, commit and push the changes
+git add README.md
+git commit -a "Update README with vowel frequency results"
+git push
